019650cbc3adb1d2274a9544d757b8d1f4989a52
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-01-19 22:46:21 +0530

    Create Search in a BST.java

---
+++ BST/Search in a BST.java
@@ -0,0 +1,48 @@
+/**
+******************************************* JAVA ***********************************/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val) return root;
+
+        if(root.val > val) return searchBST(root.left, val);
+        else return searchBST(root.right, val);
+    }
+}
+
+/************************************************ C++ ***************************************/
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL || root->val == val){
+            return root;
+        }
+
+        if(root->val > val) return searchBST(root->left,val);
+        else return searchBST(root->right, val);
+    }
+}; 
