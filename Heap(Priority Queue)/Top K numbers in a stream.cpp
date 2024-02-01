c55c5766a5cae14dafebf600dbbf26ff4ce5c2b7
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-01 22:38:05 +0530

    Update Top K numbers in a stream.cpp

--- Heap(Priority Queue)/Top K numbers in a stream.cpp
+++ Heap(Priority Queue)/Top K numbers in a stream.cpp
@@ -2,6 +2,49 @@
    gfg Qn .
    NOTE:     gives TLE in gfg after 904 test cases
 */
+
+
+// APPROACH - 1 (using vector and sort it)
+
+class Solution {
+  public:
+    static bool myComparator(pair<int,int> a, pair<int,int> b){
+        if(a.second == b.second) return a.first < b.first;
+        return a.second > b.second; // {element, frequency>
+    };
+    vector<vector<int>> kTop(vector<int>& arr, int N, int K) {
+        
+        map<int, int> mp;
+        
+        vector<vector<int>> res;
+        
+        for(int num : arr){
+            if(num == 0) continue;
+            mp[num]++;
+            
+            vector<int> curr;
+            vector<pair<int,int>> vec;
+            for(auto x : mp){
+                vec.push_back(x);
+            }
+            sort(vec.begin(), vec.end(), myComparator);
+            if(vec.size() < K){
+                for(int i=0; i < vec.size(); i++){
+                    curr.push_back(vec[i].first);
+                }
+            }else{
+                for(int i=0; i <  K; i++){
+                    curr.push_back(vec[i].first);
+                }
+            }
+            res.push_back(curr);
+        }
+        
+        return res;
+    }
+};
+
+//APPROACH -2 : using heap
 struct Compare{
     public:
        bool operator()(pair<int,int> below, pair<int,int> above){
