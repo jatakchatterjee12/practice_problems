bbd32fb917da2c97e2e4b288365e787ca6cd3e7e
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-04 11:51:56 +0530

    Create Move Zeros.java

---
+++ Arrays/Leetcode Easy/Move Zeros.java
@@ -0,0 +1,36 @@
+//*************************** C++ ***********************************//
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int snowBallSize = 0;
+        for(int i =0; i < nums.size(); i++){
+            if(nums[i] == 0) {
+                snowBallSize++;
+            }
+            else if(snowBallSize > 0){
+                int t = nums[i];
+                nums[i] = 0;
+                nums[i - snowBallSize] = t;
+            }
+        }
+    }
+};
+
+//******************************* JAVA ******************************//
+
+class Solution {
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
+}
+        
