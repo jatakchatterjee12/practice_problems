abdec61d3e73355931f19bb2656df99348389025
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-07 19:34:41 +0530

    Create Search in Rotated Sorted Array II.java

---
+++ Arrays/Binary Search/Search in Rotated Sorted Array II.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+      int n = nums.length;
+      int left = 0;
+      int right = n-1;
+      
+      while(left <= right){
+
+        int mid= left + (right - left) / 2;
+        if(nums[mid] == target)
+           return true;
+
+        if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+            left++;
+            right--;
+            continue;
+        }   
+        
+        if(nums[mid] >= nums[left]) { // left half is sorted
+          if(target >= nums[left] && target <= nums[mid]) // target lies in the left half
+            {
+                right = mid - 1;
+            }
+           else left = mid + 1; // otherwise target lies on the other half
+        } 
+        else { // right half is sorted
+          if(target >= nums[mid] && target <= nums[right]) 
+            left = mid + 1;
+          else right = mid - 1;
+        }
+        
+      }
+      return false;
+    }
+}
