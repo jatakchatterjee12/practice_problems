6471c4c54ef4c26a4fabfabd433348c7bcd984d2
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-12 10:55:48 +0530

    Create Kth Smallest Element in a BST.java

---
+++ BST/Kth Smallest Element in a BST.java
@@ -0,0 +1,61 @@
+//Approach 1:
+public int kthSmallest(TreeNode root, int k) {
+      Stack<TreeNode> st = new Stack<>();
+      
+      while (root != null) {
+          st.push(root);
+          root = root.left;
+      }
+          
+      while (k != 0) {
+          TreeNode n = st.pop();
+          k--;
+          if (k == 0) return n.val;
+          TreeNode right = n.right;
+          while (right != null) {
+              st.push(right);
+              right = right.left;
+          }
+      }
+      
+      return -1; // never hit if k is valid
+}
+
+//Approach 2: 
+  // better keep these two variables in a wrapper class
+  private static int number = 0;
+  private static int count = 0;
+
+  public int kthSmallest(TreeNode root, int k) {
+      count = k;
+      helper(root);
+      return number;
+  }
+  
+  public void helper(TreeNode n) {
+      if (n.left != null) helper(n.left);
+      count--;
+      if (count == 0) {
+          number = n.val;
+          return;
+      }
+      if (n.right != null) helper(n.right);
+  }
+
+//Approach 3:
+
+class Solution {
+    void  solve(TreeNode root, int k, List<Integer> list){
+        if(root == null) return;
+
+        solve(root.left, k, list);
+        if(list.size() != k) list.add(root.val);
+
+        if(list.size() !=  k) solve(root.right, k, list);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        solve(root, k, list);
+        return list.get(k-1);
+    }
+}
