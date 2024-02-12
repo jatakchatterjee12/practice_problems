fe35483aeb6dcb33561249dbdc3770309e1abb04
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-12 20:37:34 +0530

    Update Majority Element(Moore's Voting).cpp

--- Arrays/Majority Element(Moore's Voting).cpp
+++ Arrays/Majority Element(Moore's Voting).cpp
@@ -1,6 +1,5 @@
 /*                                    Scroll below to see JAVA code also                                */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Q6L5SoS-fTo
     Company Tags                : Amazon, Accolite, D-E-Shaw, FactSet, MakeMyTrip, Microsoft, Samsung
     Leetcode Qn Link            : https://leetcode.com/problems/majority-element/
 */
