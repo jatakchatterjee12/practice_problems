c82452655cf1d7dc4a0509c3a85bc16c1ab0e47e
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-13 12:43:06 +0530

    Update Clone an undirected Graph.cpp

--- Graph/Clone an undirected Graph.cpp
+++ Graph/Clone an undirected Graph.cpp
@@ -54,26 +54,26 @@ public:
 };
 
 //******************************************** JAVA *********************************************************//
-/*
-    class Node{
-        int val;
-        ArrayList<Node> neighbors;
-        public Node(){
-            val = 0;
-            neighbors = new ArrayList<>();
-        }
+// /*
+//     class Node{
+//         int val;
+//         ArrayList<Node> neighbors;
+//         public Node(){
+//             val = 0;
+//             neighbors = new ArrayList<>();
+//         }
     
-        public Node(int val){
-            this.val = val;
-            neighbors = new ArrayList<>();
-        }
+//         public Node(int val){
+//             this.val = val;
+//             neighbors = new ArrayList<>();
+//         }
     
-        public Node(int val, ArrayList<Node> neighbors){
-            this.val = val;
-            this.neighbors = neighbors;
-        }
-    }
-*/
+//         public Node(int val, ArrayList<Node> neighbors){
+//             this.val = val;
+//             this.neighbors = neighbors;
+//         }
+//     }
+// */
 class Solution{
     Map<Integer, Node> vis = new HashMap<>();
     void dfs(Node myNode, Node org_node){
