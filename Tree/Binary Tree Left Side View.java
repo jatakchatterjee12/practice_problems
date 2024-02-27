c4be0ad7433554fdeedbb730b20998274f914c04
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-02-27 10:54:36 +0530

    Update Binary Tree Left Side View.java

--- Tree/Binary Tree Left Side View.java
+++ Tree/Binary Tree Left Side View.java
@@ -1,4 +1,4 @@
-// DFS -> Moderated Preorder Traversal
+//Approach 1 : DFS -> Moderated Preorder Traversal
 class Solution {
   void solve(TreeNode root, int level, List<Integer> result){
     if(root == null) return;
@@ -15,3 +15,52 @@ class Solution {
       return result;
     }
 }
+
+//Approach 2 : Level Order Traversal
+
+
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+    vector<int> ans;
+    if(!root) return ans;
+    
+   queue<Node*> q;
+   q.push(root);
+
+   while(!q.empty()) {
+       
+       //first element of current level
+       ans.push_back(q.front()->data);
+       
+       int n = q.size();
+       while(n--) {
+           Node* tmp = q.front();
+           q.pop();
+           
+           if(tmp->left){ // go left
+               q.push(tmp->left);
+           }
+           if(tmp->right) { // go right
+               q.push(tmp->right);
+           }
+       }
+   }
+   return ans;
+}
+
