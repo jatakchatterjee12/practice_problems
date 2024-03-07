199c69d7ac62680597c0b3e1e54e75b070322816
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-03-07 09:53:39 +0530

    Create Middle of the Linked List.java

---
+++ Linked_List/Middle of the Linked List.java
@@ -0,0 +1,44 @@
+/*
+    Company Tags                : Adobe, Amazon, Flipkart, GE, Hike, IgniteWorld, MAQ Software, Microsoft, Morgan Stanley, 
+                                  Nagarro, Netskope, Payu, Qualcomm, Samsung, SAP Labs, Veritas, VMWare, Wipro, Zoho
+    Leetcode Link               : https://leetcode.com/problems/middle-of-the-linked-list/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+*/
+
+
+/****************************************************** C++ ******************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* first  = head;
+        ListNode* second = head;
+        
+        while(first && second && second->next) {
+            first = first->next;
+            second = second->next->next;
+        }
+        
+        return first;
+    }
+};
+
+
+
+/****************************************************** JAVA ******************************************************/
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode first = head;
+        ListNode second = head;
+
+        while (first != null && second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+        }
+
+        return first;
+    }
+}
