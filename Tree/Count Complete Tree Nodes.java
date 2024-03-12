ed0d505405380bd6651124ab0ece697181766266
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-03-12 11:45:19 +0530

    Update Count Complete Tree Nodes.java

--- Tree/Count Complete Tree Nodes.java
+++ Tree/Count Complete Tree Nodes.java
@@ -1,33 +1,92 @@
-
+/*
+    Company Tags               : Amazon, Microsoft, Salesforce, Facebook
+    Leetcode Link              : https://leetcode.com/problems/count-complete-tree-nodes/
+*/
 
 /*
-    1. If left sub tree height equals right sub tree height then,
-        a. left sub tree is perfect binary tree
-        b. right sub tree is complete binary tree
-
-    2. If left sub tree height greater than right sub tree height then,
-        a. left sub tree is complete binary tree
-        b. right sub tree is perfect binary tree
+    1. if leftHeight == rightheight --> perfect binary tree .. direct count nodes thru formula (2^h - 1)
+    2. if leftHeight != rightHeight --> count left nodes + count rightnodes + 1(for root itself)
 */
+
+//Time Complexity : O(log(n) * log(n))
+class Solution {
+public:
+    int leftHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int lh = 0;
+        TreeNode* temp = root;
+        while(temp) {
+            temp = temp->left;
+            lh++;
+        }
+        return lh;
+    }
+    int rightHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int rh = 0;
+        TreeNode* temp = root;
+        while(temp) {
+            temp = temp->right;
+            rh++;
+        }
+        return rh;
+    }
+    int countNodes(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        if(lh == rh) {
+            return pow(2, lh) - 1;
+        }
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
+//************************************************* JAVA *****************************************************//
 
 class Solution {
     public int countNodes(TreeNode root) {
         
         if(root == null) return 0;
 
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
+        int left = getLeftHeight(root);
+        int right = getRightHeight(root);
 
-        if(leftHeight == rightHeight) {
-            return (int)(Math.pow(2, leftHeight)) + countNodes(root.right);
+        if(left == right) {
+             // perfect binary tree
+             return (1 << left) - 1; // 2^left - 1 
         }
-        else {
-            return (int)(Math.pow(2, rightHeight)) + countNodes(root.left);
+
+        else return countNodes(root.left) + countNodes(root.right) + 1;
+
+    }
+    int getLeftHeight(TreeNode root) {
+        if(root == null) return 0;
+
+        int cnt = 0;
+        while(root != null){
+            cnt++;
+            root = root.left;
         }
+        return cnt;
     }
 
-    int height(TreeNode root) {
+    int getRightHeight(TreeNode root) {
         if(root == null) return 0;
-        return 1 + Math.max(height(root.left), height(root.right));
+
+        int cnt = 0;
+        while(root != null){
+            cnt++;
+            root = root.right;
+        }
+        return cnt;
     }
 }
+
