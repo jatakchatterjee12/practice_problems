297cd2ee354729c3868a4f028fb19559202c13b5
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-03-15 14:43:21 +0530

    Create Palindrome Number.java

---
+++ Arrays/Leetcode Easy/Palindrome Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int rev = 0;
+        int num = x;
+
+        while(num > 0) {
+            int rem = num%10;
+            rev = rev*10 + rem;
+            num = num/10;
+        }
+        return rev == x;
+    }
+}
