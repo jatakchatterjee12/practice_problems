11c9e0f18163ce24767cc642d63ab5ec7796a4a2
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-03-18 01:23:14 +0530

    Create Count Substrings Starting and Ending with Given Character.cpp

---
+++ strings/Count Substrings Starting and Ending with Given Character.cpp
@@ -0,0 +1,84 @@
+/*
+    Company Tags                : update soon
+    Leetcode Link               : https://leetcode.com/problems/count-substrings-starting-and-ending-with-given-character/
+*/
+
+/*************************************************************** C++ ***************************************************************
+//Approach-1 (Straight forward count)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    long long countSubstrings(string s, char c) {
+        long long count = 0;
+        long long substrings = 0;
+        
+        for(char &ch : s) {
+            if(ch == c) {
+                substrings += (1 + count);
+                count++;
+            }
+        }
+        
+        
+        return substrings;
+    }
+};
+
+
+//Approach-2 (Using simple math)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    long long countSubstrings(string s, char c) {
+        long long count = 0;
+        
+        for(char &ch : s) {
+            if(ch == c) {
+                count++;
+            }
+        }
+        
+        
+        return count *(count-1)/2 + count;
+    }
+};
+
+
+/*************************************************************** JAVA ***************************************************************
+//Approach-1 (Straight forward count)
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public long countSubstrings(String s, char c) {
+        long count = 0;
+        long substrings = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == c) {
+                substrings += (1 + count);
+                count++;
+            }
+        }
+        
+        return substrings;
+    }
+}
+
+//Approach-2 (Using simple math)
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public long countSubstrings(String s, char c) {
+        long count = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == c) {
+                count++;
+            }
+        }
+        
+        return count * (count - 1) / 2 + count;
+    }
+}
