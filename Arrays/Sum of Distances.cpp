2d2eb2abf4517a52c6028d2a00cf167238a38b11
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-03-26 02:35:54 +0530

    Added JAVA Code

--- Arrays/Sum of Distances.cpp
+++ Arrays/Sum of Distances.cpp
@@ -34,3 +34,46 @@ public:
         
     }
 };
+
+
+//************************************************************* JAVA *******************************************************//
+class Solution {
+    public long[] distance(int[] nums) {
+        
+        int n = nums.length;
+        long[] ans = new long[n];
+        Map<Integer, List<Integer>> mp = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            mp.computeIfAbsent(nums[i], k-> new ArrayList<>()).add(i);
+
+        }
+
+        for(Map.Entry<Integer, List<Integer>> it : mp.entrySet()) {
+
+            int num = it.getKey();
+            List<Integer> indices = it.getValue();
+
+            long totSum = 0;
+            for(int index : indices) totSum += (long)index;
+
+            long preSum = 0;
+
+            for(int i = 0; i < indices.size(); i++) {
+
+                int currIndex = indices.get(i);
+                long postSum = totSum - preSum - currIndex;
+
+                ans[currIndex] += (currIndex * (long)i);
+                ans[currIndex] -= preSum;
+
+                ans[currIndex] += postSum;
+                ans[currIndex] -= (currIndex * (long)(indices.size() - i - 1));
+
+                preSum += currIndex; 
+            }
+
+        }
+        return ans;
+    }
+}
