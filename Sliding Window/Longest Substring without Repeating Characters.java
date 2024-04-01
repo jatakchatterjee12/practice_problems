4b1bc663fe315b3e5d37d4e6170704b403a39072
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-04-01 13:30:21 +0530

    Create Longest Substring without Repeating Characters.java

---
+++ Sliding Window/Longest Substring without Repeating Characters.java
@@ -0,0 +1,52 @@
+//************************************************** C++ ************************************************************88//
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int n = s.length();
+        vector<bool> count(256,0);
+
+        int i=0,j=0;
+        int maxL = 0;
+
+        while(j<n){
+
+            while(count[s[j]] == 1){
+                count[s[i]] = 0;
+                i++;
+            }
+            maxL = max(maxL, j-i+1);
+
+            count[s[j]] = 1;
+            j++;
+        }
+        return maxL;
+    }
+};
+
+
+//**************************************************** JAVA ****************************************************************************//
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int[] count = new int[256];
+
+        int i = 0;
+        int j = 0;
+        int maxL = 0;
+
+        while(j < n){
+            while(count[s.charAt(j)] == 1){
+                // shrinking
+                count[s.charAt(i)] = 0;
+                i++;
+            }
+
+            maxL = Math.max(maxL, j-i+1);
+            count[s.charAt(j)] = 1;
+            j++;
+        }
+        return maxL;
+        
+    }
+}
