800f2f7b970a1be8fe16bae53763eb013cc01033
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-04-01 11:07:30 +0530

    Update Subarrays with k Different Integers.java

--- Sliding Window/Subarrays with k Different Integers.java
+++ Sliding Window/Subarrays with k Different Integers.java
@@ -1,36 +1,176 @@
+
 /*
-    LeetCode Link : https://leetcode.com/problems/subarrays-with-k-different-integers/
+    Company Tags                : Amazon
+    Leetcode Link               : https://leetcode.com/problems/subarrays-with-k-different-integers/
 */
+
+/*********************************************************************** C++ ***********************************************************/
+//Approach-1 (Standard Sliding Window twice - A very good Pattern of Sliding Window Problems)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMost(nums, k) - atMost(nums, k-1);
-    }
-
-    private int atMost(int[] nums, int k) {
-
-        int n = nums.length;
-        Map<Integer, Integer> mp = new HashMap<>(); // count
-        int i = 0;
-        int res = 0;
-
-        for(int j=0; j<n; j++) {
-
-            //if i see this item first time, k--
-            if(mp.getOrDefault(nums[j],0) == 0) k--;
-
-            mp.put(nums[j], mp.getOrDefault(nums[j],0) + 1); // mp[item]++
-
-            while(k < 0) { // shringking i
-                //k<0 ---> i have seen a new item and that exceeds the count (> k)
-
-                mp.put(nums[i], mp.getOrDefault(nums[i],0) - 1);
-                if(mp.get(nums[i]) == 0) k++;
+public:
+    
+    //Total count of subarrays having <= k distict elements
+    int slidingWindow(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        
+        int n = nums.size();
+        int i = 0; 
+        int j = 0;
+        
+        int count = 0;
+        
+        while(j < n) {
+            
+            mp[nums[j]]++;
+            
+            while(mp.size() > k) {
+                //shrink the window
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0) {
+                    mp.erase(nums[i]);
+                }
                 i++;
             }
-
-            res += j - i + 1;
-
+            
+            count += (j-i+1); //ending at j
+            j++;
         }
-        return res;
+        
+        return count;
+    }
+    
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return slidingWindow(nums, k) - slidingWindow(nums, k-1);
+    }
+};
+
+
+//Approach-2 (One Pass Flow)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        int i_chota  = 0;
+        int j        = 0;
+        int i_bada   = 0;
+        
+        int result   = 0;
+  
+        while(j < n) {
+            mp[nums[j]]++;
+            
+            while(mp.size() > k) {
+                mp[nums[i_chota]]--;
+                if(mp[nums[i_chota]] == 0) {
+                    mp.erase(nums[i_chota]);
+                }
+                i_chota++;
+                i_bada = i_chota;
+            }
+            
+            while(mp[nums[i_chota]] > 1) {
+                mp[nums[i_chota]]--;
+                i_chota++;
+            }
+            
+            if(mp.size() == k) {
+                result += i_chota - i_bada + 1;
+            }
+            j++;
+        }
+        
+        return result;
+    }
+};
+
+/*********************************************************************** JAVA ***********************************************************/
+//Approach-1 (Standard Sliding Window twice - A very good Pattern of Sliding Window Problems)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    
+    // Total count of subarrays having <= k distinct elements
+    public int slidingWindow(int[] nums, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        int n = nums.length;
+        int i = 0; 
+        int j = 0;
+        
+        int count = 0;
+        
+        while(j < n) {
+            
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            
+            while(mp.size() > k) {
+                // Shrink the window
+                mp.put(nums[i], mp.get(nums[i]) - 1);
+                if(mp.get(nums[i]) == 0) {
+                    mp.remove(nums[i]);
+                }
+                i++;
+            }
+            
+            count += (j - i + 1); // Ending at j
+            j++;
+        }
+        
+        return count;
+    }
+    
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return slidingWindow(nums, k) - slidingWindow(nums, k - 1);
+    }
+}
+
+
+
+//Approach-2 (One Pass Flow)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int n = nums.length;
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        int i_chota = 0;
+        int j = 0;
+        int i_bada = 0;
+        
+        int result = 0;
+  
+        while(j < n) {
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            
+            while(mp.size() > k) {
+                mp.put(nums[i_chota], mp.get(nums[i_chota]) - 1);
+                if(mp.get(nums[i_chota]) == 0) {
+                    mp.remove(nums[i_chota]);
+                }
+                i_chota++;
+                i_bada = i_chota;
+            }
+            
+            while(mp.get(nums[i_chota]) > 1) {
+                mp.put(nums[i_chota], mp.get(nums[i_chota]) - 1);
+                i_chota++;
+            }
+            
+            if(mp.size() == k) {
+                result += i_chota - i_bada + 1;
+            }
+            j++;
+        }
+        
+        return result;
     }
 }
