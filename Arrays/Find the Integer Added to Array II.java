cffcfa7ca266ca711d487ec3f10c44b1c033c96a
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-04-29 09:58:54 +0530

    Update Find the Integer Added to Array II.java

--- Arrays/Find the Integer Added to Array II.java
+++ Arrays/Find the Integer Added to Array II.java
@@ -3,6 +3,8 @@
     LeetCode Link         :  https://leetcode.com/problems/find-the-integer-added-to-array-ii/
 */
 
+
+// O(n^2) 
 class Solution {
     boolean check(int[] nums1, int[] nums2, int val){
         int count = 0;
@@ -35,4 +37,23 @@ class Solution {
         }
         return minEl;
     }
+}
+
+
+
+// Can be more Optimised Tc- O(n*logn)
+/*
+       1.We only have the possibility of choosing nums1[0], [1], or [2] because we can exclude any two of them.
+        
+       2.Why can't we exclude more? Because if we were to choose nums1[3], or elements greater then the condition of obtaining identical vectors will be satisfied but since nums1[i + 1] > nums[i] hence the answer wouldn't be the minimum possible number as required in the problem.
+       
+       3.Hence we wouldn't be able to satisfy the mathematical requirements of nums2.
+
+*/
+//Small change in th for loop
+for(int i = 0; i < 3; i++){
+   int val = nums2[0] - nums1[i];
+   if(check(nums1, nums2, val)){
+       minEle = min(minEle, val);
+   }    
 }
