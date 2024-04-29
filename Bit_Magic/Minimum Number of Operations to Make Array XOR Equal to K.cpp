f625de116b088d1f09f4edae13e11b79ce096547
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-04-30 00:52:14 +0530

    Create Minimum Number of Operations to Make Array XOR Equal to K.cpp

---
+++ Bit_Magic/Minimum Number of Operations to Make Array XOR Equal to K.cpp
@@ -0,0 +1,42 @@
+/*    
+    Compnay Tags                - Amazon
+    Leetcode Link               - https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/description/
+*/
+
+
+/********************************************* C++ *********************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int totalXor = 0;
+        
+        for(int &num : nums) {
+            totalXor ^= num;
+        }
+        
+        
+        int diff = (totalXor ^ k);
+        
+        return __builtin_popcount(diff);
+        
+    }
+};
+
+
+
+/********************************************* JAVA *********************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int totalXor = 0;
+        for (int num : nums) {
+            totalXor ^= num;
+        }
+        
+        int diff = (totalXor ^ k);
+        return Integer.bitCount(diff);
+    }
+}
