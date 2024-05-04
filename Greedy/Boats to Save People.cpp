b535cd67e30dcb1d15e7550613b1004c9c6a6586
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-05-04 10:04:29 +0530

    Create Boats to Save People.cpp

---
+++ Greedy/Boats to Save People.cpp
@@ -0,0 +1,61 @@
+/*  
+    Company Tags                : Google
+    Leetcode Link               : https://leetcode.com/problems/boats-to-save-people/
+*/
+
+
+//************************************************ C++ **************************************************//
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        
+        
+        //sort the array
+        sort(begin(people), end(people));
+        
+        int i = 0, j = n-1;
+        
+        int boats = 0;
+        
+        while(i <= j) {
+            if(people[j] + people[i] <= limit) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            boats += 1;
+        }
+        
+        return boats;
+    }
+};
+
+//******************************************************* JAVA ***************************************************//
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+
+        Arrays.sort(people);
+
+        int i=0;
+        int j=n-1;
+
+        int boatCounts = 0;
+
+        while(i<=j){
+            if(people[i] + people[j] <= limit){
+                
+                j--;
+                i++;
+            }
+            else{
+                
+                j--;
+            }
+            boatCounts++;
+        }
+        return boatCounts;
+    }
+}
