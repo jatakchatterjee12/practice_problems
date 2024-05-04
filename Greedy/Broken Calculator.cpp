e5a7981c10705f544fc2fc62f640c16c289f8023
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-05-04 12:33:42 +0530

    Create Broken Calculator.cpp

---
+++ Greedy/Broken Calculator.cpp
@@ -0,0 +1,38 @@
+/*
+    Company Tags                : Samsung
+    Leetcode Link               : https://leetcode.com/problems/broken-calculator/
+*/
+
+//************************************************** C++ ************************************************//
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if(startValue >= target)
+            return startValue-target;
+        
+        //even
+        if(target%2 == 0) {
+            return 1 + brokenCalc(startValue, target/2);
+        }
+        
+        
+        return 1 + brokenCalc(startValue, target+1);
+    }
+};
+
+//********************************************************* JAVA *************************************************//
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int ans = 0;
+
+        if(startValue >= target) {
+            return startValue - target;
+        }
+
+        if(target%2 == 0){
+            return 1 + brokenCalc(startValue, target/2);
+        }
+
+        return 1 + brokenCalc(startValue, target+1);
+    }
+}
