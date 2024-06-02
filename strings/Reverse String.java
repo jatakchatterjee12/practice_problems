eb10bc3484bf6131c8fe97ea9974438d7961228f
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-06-02 23:43:45 +0530

    Create Reverse String.java

---
+++ strings/Reverse String.java
@@ -0,0 +1,16 @@
+/*
+      leetcode link    :    https://leetcode.com/problems/reverse-string/description/
+*/
+
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int n = s.length;
+        for(int i = 0; i < n/2; i++) {
+            char ch = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = ch;
+        }
+
+    }
+}
