0dc8295f82fcf75c37f0b1520374e3f3381286c0
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-06-06 07:58:20 +0530

    Update Insert Intervals.java

--- Arrays/Intervals_Based_Qns/Insert Intervals.java
+++ Arrays/Intervals_Based_Qns/Insert Intervals.java
@@ -115,26 +115,34 @@ class Solution {
 //S.C : O(n) for result
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int i = 0;
-        List<int[]> result = new ArrayList<>();
+        
         int n = intervals.length;
+        int i = 0;
 
-        while (i < n) {
-            if (intervals[i][1] < newInterval[0]) {
-                result.add(intervals[i]);
-            } else if (intervals[i][0] > newInterval[1]) {
-                break;
-            } else {
-                // Merge the intervals and check further
+        List<int[]> result = new ArrayList<>();
+
+        while(i < n) {
+
+            if(intervals[i][1] < newInterval[0]){
+                result.add(intervals[i]);   //left portion added to result
+            }
+            else if(intervals[i][0] > newInterval[1]){
+                break; //right portion that we will add to result later after inserting 
+                       //the newInterval,which should be put in between the left portion and right portion
+            }
+            else{
+                //overlap : merge karo aur age jao
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+
             }
             i++;
         }
 
-        result.add(newInterval);
-        while (i < n) {
-            result.add(intervals[i]);
+        result.add(newInterval); // adding the newInterval after merging
+
+        while(i < n) {
+            result.add(intervals[i]); // now add the left out right portion to the result
             i++;
         }
 
