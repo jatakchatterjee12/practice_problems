a246af2bd7ff07759de6a0ee5ec132f43e7164d3
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2024-06-30 22:22:23 +0530

    Update and rename DisjointSet (find,union by rank).java to DisjointSet (find,union by rank, union by Size) using array .java

--- Graph/Disjoint Set/DisjointSet (find,union by rank, union by Size) using array .java
+++ Graph/Disjoint Set/DisjointSet (find,union by rank, union by Size) using array .java
@@ -1,14 +1,17 @@
 class DSU {
     private int[] parent;
     private int[] rank;
+    private int[] size;
     
 
     public DSU(int n) {
         parent = new int[n + 1];
         rank = new int[n + 1];
+        size = new int[n + 1];
         
         for (int i = 1; i <= n; i++) {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -33,6 +36,25 @@ class DSU {
             parent[xParent] = yParent;
             rank[yParent]++;
         }
+    }
+
+    public void unionBySize(int u, int v) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent) 
+            return;
+        
+       if(size[x_parent] > size[y_parent]) {
+	        parent[y_parent] = x_parent;
+	        size[x_parent]  += size[y_parent];
+	    } else if(size[x_parent] < size[y_parent]) {
+	        parent[x_parent] = y_parent;
+	        size[y_parent]  += size[x_parent];
+	    } else {
+	        parent[x_parent] = y_parent;
+	        size[y_parent]  += size[x_parent];
+	    }
     }
 
 }
