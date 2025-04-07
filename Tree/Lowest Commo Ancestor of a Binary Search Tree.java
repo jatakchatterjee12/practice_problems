d59bed87f3a11feda6bbc8585e29ddb3805567ab
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2025-04-07 09:14:08 +0530

    Update Lowest Commo Ancestor of a Binary Search Tree.java

--- Tree/Lowest Commo Ancestor of a Binary Search Tree.java
+++ Tree/Lowest Commo Ancestor of a Binary Search Tree.java
@@ -44,3 +44,70 @@ public:
         return curr;
     }
 };
+
+
+//***************************************************** JAVA ****************************************************//
+//Approach-1 (Recursive) Time : O(N) worst case : skewed BST
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == null){
+            return root;
+        }
+
+        if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        else if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        else{
+            return root;
+        }
+    }
+}
+
+
+
+//Approach-2 (Iterative) Time : O(N) worst case : skewed BST
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr.val > p.val && curr.val > q.val){
+                curr = curr.left;
+            }
+            else if(curr.val < p.val && curr.val < q.val){
+                curr = curr.right;
+            }
+            else{
+                // we are at our answer node, no need to g further, just break the loop and return the curr node
+                break;
+            }
+            
+        }
+        return curr;
+    }
+}
