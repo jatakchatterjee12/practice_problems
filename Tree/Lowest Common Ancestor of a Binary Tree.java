679504dfdc2dfc18d1a90751574d34aa8050cd5b
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2025-04-07 08:13:52 +0530

    Update Lowest Common Ancestor of a Binary Tree.java

--- Tree/Lowest Common Ancestor of a Binary Tree.java
+++ Tree/Lowest Common Ancestor of a Binary Tree.java
@@ -1,6 +1,31 @@
+
 /*
-    LeetCode Qn Link --> https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+    Company Tags : Accolite, Amazon, American Express, Expedia, MakeMyTrip, Microsoft, Payu, Snapdeal, Times Internet, Twitter
+    Link to Qn   : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 */
+
+/********************************************************* C++ **************************************************//
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return NULL;
+        
+        if(root->val == p->val || root->val == q->val)
+            return root;
+        
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        
+        if(l && r)
+            return root;
+        
+        return l?l:r;
+    }
+};
+
+
+//****************************************************** JAVA *******************************************//
 
 /**
  * Definition for a binary tree node.
