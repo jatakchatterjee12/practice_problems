aa33b1d0d8364c3512b96d726a17abd9cadaf6b4
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2025-04-27 21:35:16 +0530

    Create Count Subarrays of a Length Three with a Condition.java

---
+++ Arrays/Leetcode Easy/Count Subarrays of a Length Three with a Condition.java
@@ -0,0 +1,46 @@
+
+/*     
+    Company Tags                : 
+    Leetcode Link               : https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/description/
+*/
+
+/*********************************************************** C++ **************************************************/
+//Approach (Striaght forward simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+
+        for(int i = 1; i < n-1; i++) {
+            if(nums[i] == (nums[i-1] + nums[i+1])*2) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+};
+
+
+/*********************************************************** JAVA **************************************************/
+//Approach (Striaght forward simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == (nums[i - 1] + nums[i + 1])*2) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
