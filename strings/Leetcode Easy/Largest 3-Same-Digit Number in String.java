dd85db6cd4e734a7d511f4105050c65cd8ec67fc
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2025-08-14 23:59:55 +0530

    Create Largest 3-Same-Digit Number in String.java

---
+++ strings/Leetcode Easy/Largest 3-Same-Digit Number in String.java
@@ -0,0 +1,60 @@
+/*
+    
+    Company Tags                : Most asked online Assessment Qn of Amazon in year 2022 (Karat, Amazon, Uber, Pinterest)
+    Leetcode Link               : https://leetcode.com/problems/largest-3-same-digit-number-in-string
+*/
+
+
+/******************************************************************** C++ ********************************************************************/
+//Simple iteration and check
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        char maxChar = ' ';
+        
+        for(int i = 2; i<num.length(); i++) {
+            if(num[i] == num[i-1] && num[i] == num[i-2]) {
+                maxChar = max(maxChar, num[i]);
+            }
+        }
+        
+        return maxChar == ' ' ? "" : string(3, maxChar);
+    }
+};
+
+
+/******************************************************************** JAVA ********************************************************************/
+//Simple iteration and check
+public class Solution {
+    public String largestGoodInteger(String num) {
+        char maxChar = ' ';
+        
+        for (int i = 2; i < num.length(); i++) {
+            if (num.charAt(i) == num.charAt(i - 1) && num.charAt(i) == num.charAt(i - 2)) {
+                maxChar = (char) Math.max(maxChar, num.charAt(i));
+            }
+        }
+        
+        return maxChar == ' ' ? "" : new String(new char[] { maxChar, maxChar, maxChar });
+    }
+}
+
+
+
+////////////////////////////////////////////
+class Solution {
+    public String largestGoodInteger(String num) {
+        
+        String res="";
+        int n = num.length();
+
+        for(int i = 1; i < n-1; i++){
+            if(num.charAt(i-1) == num.charAt(i) && num.charAt(i) == num.charAt(i+1)){
+                if(res.length() == 0 || res.charAt(0) < num.charAt(i)){
+                    res = num.substring(i-1, i+2);
+                }
+            }
+        }
+        return res;
+    }
+}
