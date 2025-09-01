13f8e2349fab604c5bd4fb2fb831779301377878
Jatak Chatterjee <90604134+jatakchatterjee12@users.noreply.github.com>
2025-09-01 08:59:59 +0530

    Update Sudoku Solver.java

--- backtrack/Sudoku Solver.java
+++ backtrack/Sudoku Solver.java
@@ -47,6 +47,70 @@ class Solution {
     }
 }
 
+
+////////////////////
+//Approach (Khandani Backtracking template and all possible options)
+//T.C : O(1), fixed frid size
+//S.C : O(1), fixed grid size
+class Solution {
+    
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char d = '1'; d <= '9'; d++) {
+                        if (isValid(board, i, j, d)) {
+                            board[i][j] = d;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            board[i][j] = '.'; // backtrack
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // sudoku solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char d) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == d) return false; // check column
+            if (board[row][i] == d) return false; // check row
+        }
+
+          //validate current box
+        /*
+            If you want to get the start index ([start_i][start_j] i.e. top left) of a box
+            given i and j
+            start_i = i/3 * 3;
+            start_j = j/3 * 3;
+            
+            Now, you can check current box
+        */
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                if (board[startRow + k][startCol + l] == d) {
+                    return false; // check 3x3 box
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
 //******************************************* C++ ******************************************************//
 class Solution {
 public:
